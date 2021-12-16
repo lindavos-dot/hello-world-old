b243a1c5e4853b923ca5270da0f032368b6ea140
lindavos-dot <lindavos123@hotmail.com>
2021-12-16 10:42:27 +0100

    Nieuw

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ __human_name__ = 'hello-world'
 
 # Add your code after this line
 print("Hello, world")
-
+print ("My name is Linda")
+print ("How are you doing?")
+print ("What a lovely day!")
+print ("one", "two", "three")
+print (1, 2, 3)
+print ("1", "2", "3")
