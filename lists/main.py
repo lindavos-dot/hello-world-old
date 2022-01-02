b467667222d927527c11e1f3de8d06eba142f223
lindavos-dot <lindavos123@hotmail.com>
2022-01-02 14:14:03 +0100

    20220102

--- lists/main.py
+++ lists/main.py
@@ -16,8 +16,9 @@ alphabetical_order(list_of_films)
 print(list_of_films)
 
 # Write a function won_golden_globe that takes a film name and returns True or False based on whether or not this movie won a Golden Globe.
+# Look into using the lower-function on the given film string.
 
-list_golden_globe = ["Jaws", "E.T. the Extra-Terrestrial", "Star Wars: Episode IV - A New Hope", "Memoirs of a Geisha", "element"]
+list_golden_globe = ["Jaws", "E.T. the Extra-Terrestrial", "Star Wars: Episode IV - A New Hope", "Memoirs of a Geisha"]
 
 def won_golden_globe(list_to_search, element):
   if element in list_to_search:
@@ -27,4 +28,18 @@ def won_golden_globe(list_to_search, element):
 
 print(won_golden_globe(list_golden_globe, "Jaws"))
 
+for element in list_golden_globe:
+  print(element.islower())
+
 # Write a function remove_toto_albums that takes a list of strings, removes Joseph's Toto albums from it and returns the tidy list. 
+
+toto_albums = ["Fahrenheit", "The Seventh One", "Toto XX", "Falling in Between", "Old Is New"]
+list_of_films.append(toto_albums)
+print(list_of_films)
+
+def remove_toto_albums(list_to_keep, delete_from_list):
+    return list_to_keep.remove(delete_from_list)
+
+remove_toto_albums(list_of_films, toto_albums)
+print(list_of_films)
+
