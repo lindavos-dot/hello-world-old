5088754bdffb6b11dd5b62745a78e5c93753cfdf
lindavos-dot <lindavos123@hotmail.com>
2022-01-03 15:59:04 +0100

    20220103

--- lists/main.py
+++ lists/main.py
@@ -10,26 +10,26 @@ __human_name__ = 'lists'
 list_of_films = ["Daddy-O", "I passed for White", "The Secret Ways", "Bachelor Flat", "Daimond Head", "Gidget Goed to Rome"]
 
 def alphabetical_order(list):
-    return list.sort()
-    
-alphabetical_order(list_of_films)
-print(list_of_films)
+    alphabetical_list = sorted(list)
+    return alphabetical_list
+
+print(alphabetical_order(list_of_films))
 
 # Write a function won_golden_globe that takes a film name and returns True or False based on whether or not this movie won a Golden Globe.
 # Look into using the lower-function on the given film string.
 
-list_golden_globe = ["Jaws", "E.T. the Extra-Terrestrial", "Star Wars: Episode IV - A New Hope", "Memoirs of a Geisha"]
+def won_golden_globe(film_name):
+    won_golden_globe = [
+        "jaws",
+        "e.t. the extra-terrestrial",
+        "Star wars: episode iv - a new hope",
+        "memoirs of a geisha",
+    ]
+    return film_name.lower() in won_golden_globe
 
-def won_golden_globe(list_to_search, element):
-  if element in list_to_search:
-    return True
-  else:
-    return False
+print(won_golden_globe("Jaws"))
+print(won_golden_globe("Jeff"))
 
-print(won_golden_globe(list_golden_globe, "Jaws"))
-
-for element in list_golden_globe:
-  print(element.lower())
 
 # Write a function remove_toto_albums that takes a list of strings, removes Joseph's Toto albums from it and returns the tidy list. 
 
@@ -38,12 +38,12 @@ list_of_films.extend(toto_albums)
 
 print(list_of_films)
 
-def remove_toto_albums(list1, list2):
-    for element in list1:
-        if element in list2:
-          list2.remove(element)
-    return list2
+def remove_toto_albums(list):
+    for element in toto_albums:
+        if element in list:
+          list.remove(element)
+    return list
 
-remove_toto_albums(toto_albums, list_of_films)
+remove_toto_albums(list_of_films)
 
 print(list_of_films)
