0539afc05dcfba336846d4dfaed9ef855a907543
lindavos-dot <lindavos123@hotmail.com>
2022-02-26 15:23:36 +0100

    20220226

---
+++ dictionariesv2/main.py
@@ -0,0 +1,28 @@
+# Do not modify these lines
+from helpers import get_countries
+
+__winc_id__ = "00a4ab32f1024f5da525307a1959958e"
+__human_name__ = "dictionariesv2"
+
+# Add your code after this line
+
+# Part 1: Create Passport
+
+def create_passport(name, date_of_birth, place_of_birth, height, nationality):
+    passport_person = {
+        "name": name,
+        "date_of_birth": date_of_birth,
+        "place_of_birth": place_of_birth,
+        "height": height,
+        "nationality": nationality,
+    }
+    return passport_person
+
+
+my_passport = create_passport("Linda Vos", "1987-9-13", "Leeuwarden", 1.70, "Netherlands")
+print(my_passport["name"])
+
+# Part 2: Add Stamp
+
+
+# Part 3: Add biometric data
