e30520545bdafa7358b35b33ebd931587ca7a062
lindavos-dot <lindavos123@hotmail.com>
2022-02-27 14:41:17 +0100

    20220227

--- dictionariesv2/main.py
+++ dictionariesv2/main.py
@@ -20,7 +20,7 @@ def create_passport(name, date_of_birth, place_of_birth, height, nationality):
 
 
 my_passport = create_passport("Linda Vos", "1986-8-19", "Amsterdam", 1.80, "Netherlands")
-print(my_passport["name"])
+# print(my_passport["name"])
 
 # Part 2: Add Stamp
 
@@ -34,12 +34,29 @@ def add_stamp(passport, country):
 
     
 add_stamp(my_passport, "Germany")
-print(my_passport)
+# print(my_passport)
 
 add_stamp(my_passport, "Belgium")
-print(my_passport)
+# print(my_passport)
 
 add_stamp(my_passport, "Netherlands")
-print(my_passport)
+# print(my_passport)
 
 # Part 3: Add biometric data
+
+def add_biometric_data(passport, name, value, date):
+    if "biometric" not in passport:
+        passport["biometric"] = {}
+    
+    description = {
+            "value": value,
+            "date": date,
+        }
+    passport["biometric"][name] = description
+    return passport
+
+
+add_biometric_data(my_passport, "hair", "in the summer her hair turns completely white", "2022-06-21")
+add_biometric_data(my_passport, "skin", "in summer her skin turns light brown", "2022-06-21")
+print(my_passport)
+
