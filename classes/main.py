218b1e52dee38dbd3d7c497513e31568f758aa83
lindavos-dot <lindavos123@hotmail.com>
2022-04-18 13:18:51 +0200

    20220418

--- classes/main.py
+++ classes/main.py
@@ -6,7 +6,7 @@ __human_name__ = 'classes'
 
 # Part 1: Players
 
-from unicodedata import name
+from turtle import speed
 
 
 class Player():
@@ -35,14 +35,64 @@ class Player():
             return "accuracy", self.accuracy
         
 
+# Part 2 Commentators
 
-player_1 = Player("Hans van Breukelen", 0.8, 0.7, 0.6)
+
+class Commentator():
+    def __init__(self, name):
+        self.name = name
+
+
+    def sum_player(self, player):
+        return player.speed + player.endurance + player.accuracy
+
+
+    def compare_players(self, player_1, player_2, skill):
+        compare_player_1 = getattr(player_1, skill)
+        compare_player_2 = getattr(player_2, skill)
+        
+        strenght_player_1 = player_1.strength() 
+        strenght_player_2 = player_2.strength()
+
+        sum_player_1 = self.sum_player(player_1)
+        sum_player_2 = self.sum_player(player_2)
+
+        if compare_player_1 > compare_player_2:
+            return player_1.name
+        elif compare_player_1 < compare_player_2:
+            return player_2.name
+        
+        else:
+            if strenght_player_1 > strenght_player_2:
+                return player_1.name
+            elif strenght_player_1 < strenght_player_2:
+                return player_2.name
+            
+            else:
+                if sum_player_1 > sum_player_2:
+                    return player_1.name
+                elif sum_player_1 < sum_player_2:
+                    return player_2.name
+        
+                else:
+                    return "These two players might as well be twins!"
+
+ray = Commentator('Ray Hudson')
+# print(ray.name)
+
+
+player_1 = Player("Hans van Breukelen", 0.8, 0.7, 0.5)
 # print(player_1.__dict__)
 # print(player_1.introduce())
 
-player_2 = Player("Frank de Boer", 0.7, 0.8, 0.9)
+player_2 = Player("Frank de Boer", 0.8, 0.7, 0.6)
 # print(player_2.__dict__)
 # print(player_2.strength())
 
 player_3 = Player("Ronald de Boer", 0.7, 0.8, 0.9)
 # print(player_3.__dict__)
+
+ray = Commentator("Ray Hudson")
+print(ray.sum_player(player_2))
+print(ray.compare_players(player_1, player_2, "accuracy"))
+
